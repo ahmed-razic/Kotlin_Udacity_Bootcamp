317e3c663335fa58e0ef8789f0bddacaa684e76a
ahmed-razic <ahmed.razic@gmail.com>
2021-01-08 17:41:51 +0100

    First commit

---
+++ src/main/kotlin/functions/Practice2.kt
@@ -0,0 +1,5 @@
+package functions
+
+fun main(args: Array<String>){
+    println(if (args[0].toInt() < 12) "Good morning, Kotlin" else "Good night, Kotlin")
+}
---
+++ src/main/kotlin/functions/Practice3.kt
@@ -0,0 +1,17 @@
+package functions
+import java.util.*
+
+fun main(){
+    feedTheFish()
+}
+
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
