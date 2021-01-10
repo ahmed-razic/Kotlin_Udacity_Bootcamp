06385a3fe5091b8df2140047d9417e8e1dc7e5f4
ahmed-razic <ahmed.razic@gmail.com>
2021-01-10 10:36:32 +0100

    Added Delegates

--- src/main/kotlin/classes/practice5.kt
+++ src/main/kotlin/classes/practice5.kt
@@ -1,2 +1,73 @@
 package classes
 
+
+
+abstract class FoodSpice(val name: String, private val spiciness: String = "mild", private val spiceColor: SpiceColor) : SpiceColor by spiceColor {
+
+    val heat: Int
+        get() {
+            return when (spiciness) {
+                "mild" -> 1
+                "medium" -> 3
+                "spicy" -> 5
+                "very spicy" -> 7
+                "extremely spicy" -> 10
+                else -> 0
+            }
+        }
+    abstract fun prepareSpice()
+}
+
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: String
+}
+
+object YellowSpice : SpiceColor {
+    override val color = "yellow"
+}
+
+class Curry(name: String, spiciness: String, color: SpiceColor = YellowSpice) : FoodSpice(name, spiciness, color), Grinder {
+
+    override fun prepareSpice() {
+        grind()
+    }
+
+    override fun grind() {
+    }
+}
+
+fun main(){
+    val curry = Curry("Curry", "very spicy")
+    println("Name of spice is ${ curry.name }.")
+    println("Spiciness of curry is ${ curry.heat }.")
+    println("Color of spice is ${ curry.color }.")
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+val spices1 = listOf(
+    FoodSpice("curry", "mild"),
+    FoodSpice("pepper", "medium"),
+    FoodSpice("cayenne", "spicy"),
+    FoodSpice("ginger", "mild"),
+    FoodSpice("red curry", "medium"),
+    FoodSpice("green curry", "mild"),
+    FoodSpice("hot pepper", "extremely spicy")
+)
+val spice = FoodSpice("cayenne", spiciness = "spicy")
+val spicelist = spices1.filter {it.heat < 5}
+
+fun makeSalt() = Spice("Salt")*/
